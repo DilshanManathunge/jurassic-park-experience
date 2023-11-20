66ec0d2781b297766b8567842a3505b6de1b69a5
Dilshan Manathunge <dilshan.manathunge@gmail.com>
2023-11-20 21:40:20 +0530

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="page-view-main">
+    <p class="text-white">Explore the Park... on y(our) own terms!</p>
+    <h1 class="text-8xl text-orange-500 header-title">Jurassic Park trail</h1>
+    <p class="text-yellow-100 text-center w-1/2">
+      Located in a secluded island off the cost of Costa Rica,surrounded by
+      sandy shores filled with curious small green critters to the luscious
+      forests guarded with windy roaring mountains and gaint roaming reptiles.
+      Our organization is dedicated to providing those adventours folks such as
+      yourself the true jurassic park...minus the missing limb or two!
+    </p>
+    <button class="mt-32 bg-orange-500 px-12 py-4 action-button">
+      Your Adventure awaits
+    </button>
+  </div>
+  <div class="page-view grid grid-cols-2">
+    <div class="flex flex-col justify-center items-center">
+      <h1 class="text-8xl text-lime-950 font-black text-center">
+        <span style="font-size: 8rem">04</span><br />
+        <span style="font-family: : 'Roboto', sans-serif;"
+          >UNIQUE<br />
+          TASTES</span
+        >
+      </h1>
+      <p>
+        We have curated 4 unique adventures to suit the unique taste of our
+        clientel
+      </p>
+    </div>
+    <div class="flex flex-col justify-center items-center">image 1</div>
+  </div>
+  <div class="page-view">Section 3</div>
+</template>
+<script setup>
+import { onMounted } from "vue"
+
+onMounted(() => {
+  console.log("home is mounted")
+})
+</script>
+<style>
+.action-button {
+  outline: 0.1rem solid rgb(255, 166, 0);
+  outline-offset: 1rem;
+  -moz-outline-radius: 0rem;
+}
+.header-title {
+  font-family: Jurassic Park;
+  font-weight: 400;
+}
+.page-view-main {
+  padding: 0vh 20vh;
+  font-family: "Roboto", sans-serif;
+  font-weight: 500;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 95vh;
+  background-color: rgb(23 24 23);
+}
+.page-view {
+  padding: 0vh 20vh;
+
+  height: 100vh;
+  background-color: rgb(255 239 228);
+}
+</style>
